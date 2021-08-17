94059773c2bdf58934521c11743d9fa22a35ed4b
Adiyatma Koto <adiyatma.padang@gmail.com>
2021-08-17 13:13:35 +0700

    adit commit repair

--- rfid_read.py
+++ rfid_read.py
@@ -4,9 +4,9 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-        text = input('New data:')
-        print("Now place your tag to write")
-        reader.write(text)
-        print("Written")
+    text = input('New data:')
+    print("Now place your tag to write")
+    reader.write(text)
+    print("Written")
 finally:
         GPIO.cleanup()
